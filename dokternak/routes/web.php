818ac835239a311c56a1a264073f886ffab3fb82
defitamara <71813396+defitamara@users.noreply.github.com>
2021-04-01 19:34:15 +0700

    Backend dokternak

--- dokternak/routes/web.php
+++ dokternak/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk Backend ----------------------------------------------------
+Route::group(['namespace' => 'Backend'], function()
+{
+    Route::resource('dashboard', 'DashboardController');
+});
+// ------------------------------------------------------------------------
