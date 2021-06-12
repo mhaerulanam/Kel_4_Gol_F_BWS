283931263de348e5e072b3b131a4dc442b435087
ardanvf <71808926+ardanvf@users.noreply.github.com>
2021-06-12 14:43:04 +0700

    Tombol

---
+++ UJI COBA/dokternak/app/Http/Controllers/frontend/DetailDokterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+use App\Http\Controllers\controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DetailDokterController extends Controller
+{
+    //
+    public function index()
+    {
+        $dokter = DB::table('dokter')
+        ->join('jabatan','dokter.id_jabatan', '=', 'jabatan.jabatan')
+        ->select(*)
+        ->get();
+
+
+
+        return view('frontend.detaildokter', compact('dokter'))->('dokter',$dokter);
+
+    }
+
+    public function detail($id) {
+        return view('frontend.detaildokter',compact('dokter'));
+    }
+
+}
+        // $peternak = DB::table('peternak')->get();
+        // return view('backend.peternak.index',compact('peternak'));
+
+        // $jabatans = DB::table('jabatan')->get();
+        // return view ('frontend.detaildokter', compact('jabatan'))->with('jabatan',$jabatans);
+        //,compact('tutorial'))->with('tutorial', $tutorial);
+        // return view('frontend.detaildokter', compact('jabatan'))->with('jabatan',$jabatan);
