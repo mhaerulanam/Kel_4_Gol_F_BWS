1aaa02e9e372ffdc8e940ce24470c8d06dc2463e
Defi Tamara <defytamara2610@gmail.com>
2021-06-20 16:08:35 +0700

    CRUD Data Rekam Medik
    Tombol Edit dan Hapus telah berfungsi dengan baik

--- UJI COBA/dokternak/app/Http/Controllers/petugas/RekammedikController.php
+++ UJI COBA/dokternak/app/Http/Controllers/petugas/RekammedikController.php
@@ -56,51 +56,34 @@ class RekammedikController extends Controller
 
         rekam_medik::create($data_simpan);
 
-        return redirect()->route('index')
-                        ->with('success','Data rekam medik baru telah berhasil disimpan');
+        return redirect()->back()->with('success','Data Rekam Medik baru telah berhasil disimpan');
     }
 
-    public function edit($id_rmd)
+    public function edit(Request $request, $id_rmd)
     {
-        $rekam_medik = rekam_medik::where('id_rmd',$id_rmd)->first();
-        return view('petugas.rekam_medik.index',compact('rekam_medik'));
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+
+            rekam_medik::where(['id_rmd'=>$id_rmd])->update(['tanggal'=>$data['tanggal'], 
+            'id_kategori'=>$data['id_kategori'],
+            'id_ktg'=>$data['id_ktg'],
+            'nama_hewan'=>$data['nama_hewan'],
+            'nama_peternak' =>$data['nama_peternak'],
+            'alamat'=>$data['alamat'],
+            'keluhan'=>$data['keluhan'],
+            'diagnosa'=>$data['diagnosa'],
+            'pelayanan'=>$data['pelayanan']]);
+
+            return redirect()->back()->with('success','Data Rekam Medik telah berhasil diperbarui');
+        }
+
+    
     }
 
-    public function update(Request $request, $id_rmd)
+    public function delete($id_rmd)
     {
-
-        $message = [
-            'numeric' => ':attributer harus diisi nomor.'
-        ];
-
-        $validator = FacadesValidator::make($request->all(),[
-            // 'nama' => 'required|string|max:100',
-            // 'tingkatan' => 'required|numeric',
-        ], $message)->validate();
-
-        $data_simpan = [
-            'tanggal' => $request->tanggal,
-            'id_kategori' => $request->id_kategori, 
-            'id_ktg' => $request->id_ktg, 
-            'nama_hewan' => $request->nama_hewan, 
-            'nama_peternak' => $request->nama_peternak,
-            'alamat' => $request->alamat,
-            'keluhan' => $request->keluhan,
-            'diagnosa' => $request->diagnosa,
-            'pelayanan' => $request->pelayanan,
-        ];
-
-        rekam_medik::where('id_rmd', $id_rmd)->update($data_simpan);
-
-        return redirect()->route('index')
-                        ->with('success','Data rekam_medik telah berhasil diperbarui');
-    }
-
-    public function destroy($id_rmd)
-    {
-        $rekam_medik = rekam_medik::where('id_rmd',$id_rmd)->delete();
-        return redirect()->route('index')
-                        ->with('success','Data rekam medik telah berhasil dihapus');
+        rekam_medik::where(['id_rmd'=>$id_rmd])->delete();
+        return redirect()->back()->with('success','Data rekam medik telah berhasil dihapus');
     }
 
 }
