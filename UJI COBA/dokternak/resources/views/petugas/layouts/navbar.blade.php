a9aab0a7e5b51b1a163c51854c66b9fd4f9550a1
WikeeSriW <71832042+WikeeSriW@users.noreply.github.com>
2021-07-16 23:52:18 +0700

    ubah password (petugas)

--- UJI COBA/dokternak/resources/views/petugas/layouts/navbar.blade.php
+++ UJI COBA/dokternak/resources/views/petugas/layouts/navbar.blade.php
@@ -43,6 +43,7 @@
                                         </a>
         
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a href="/petugas/change-password"> Ubah Password </a>
                                             <a class="dropdown-item" href="{{ route('logout') }}"
                                                onclick="event.preventDefault();
                                                              document.getElementById('logout-form').submit();">
