7a580dae39e8b2307a4ba287687171e22998b104
Defi Tamara <defytamara2610@gmail.com>
2021-07-20 22:19:37 +0700

    Update index.blade.php

--- UJI COBA/dokternak/resources/views/petugas/data_obat/index.blade.php
+++ UJI COBA/dokternak/resources/views/petugas/data_obat/index.blade.php
@@ -276,7 +276,7 @@ $(document).ready(function(){
                     </div>
                 </div>
             </div>
-            <div class="table-filter">
+            {{-- <div class="table-filter">
                 <div class="row">
                     <div class="col-sm-3">
                         <div class="show-entries">
@@ -317,10 +317,10 @@ $(document).ready(function(){
                                 <option>Kuda</option>
                             </select>
                         </div> --}}
-                        <span class="filter-icon"><i class="fa fa-filter"></i></span>
+                        {{-- <span class="filter-icon"><i class="fa fa-filter"></i></span>
                     </div>
                 </div>
-            </div>
+            </div> --}} 
             <table class="table table-striped table-hover">
                 <thead>
                     <tr>
@@ -361,7 +361,7 @@ $(document).ready(function(){
                 </tbody>
             </table>
             <div class="clearfix">
-                <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+                {{-- <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div> --}}
                 <ul class="pagination">
                     {{-- <li class="page-item disabled"><a href="#">Previous</a></li>
                     <li class="page-item active"><a href="#" class="page-link">1</a></li>
