69d8c0b3dd29f9d6a2359b00359122a2b9445ab9
WikeeSriW <71832042+WikeeSriW@users.noreply.github.com>
2021-07-21 10:20:32 +0700

    Update ApiPuskeswanController.php

--- UJI COBA/dokternak/app/Http/Controllers/ApiPuskeswanController.php
+++ UJI COBA/dokternak/app/Http/Controllers/ApiPuskeswanController.php
@@ -10,13 +10,21 @@ class ApiPuskeswanController extends Controller
     public function getAll() 
     {
         $puskeswan = Puskeswan::all();
-        return response()->json($puskeswan, 201);
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Puskeswan Berhasil Ditampilkan Semua',
+            'data' => $puskeswan
+        ], 201);
     }
 
     public function getPuskeswan($id) 
     {
         $puskeswan = Puskeswan::find($id);
-        return response()->json($puskeswan, 200);
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Puskeswan Berhasil Ditampilkan per-item',
+            'data' => $puskeswan
+        ], 200);
     }
 
     public function createPuskeswan(Request $request) 
@@ -47,5 +55,19 @@ class ApiPuskeswanController extends Controller
             'status' => 'ok',
             'message' => 'Puskeswan Berhasil Dihapus!'
         ], 201);
+    }
+
+    public function cariPuskeswan(Request $request){
+        $cari = $request->nama_puskeswan;
+         //mengambul data dari tabel dokter sesuai pencarian data
+        //  $petugas =  Dokter::where('nama_dokter','like',"%".$request."%");
+        $puskeswan =  Puskeswan::where('nama_puskeswan','like',"%".$cari."%")->get();
+
+
+            return response()->json([
+            'status' => 'ok',
+            'message' => 'Puskeswan yang dicari Berhasil Ditampilkan',
+            'data' => $puskeswan
+        ], 201);    
     }
 }
