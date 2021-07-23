c7fbe4b11e53f8b92557b63555f4c9f07d96ad21
Defi Tamara <defytamara2610@gmail.com>
2021-07-23 20:22:02 +0700

    Update detaildokter.blade.php

--- UJI COBA/dokternak/resources/views/frontend/detaildokter.blade.php
+++ UJI COBA/dokternak/resources/views/frontend/detaildokter.blade.php
@@ -69,8 +69,8 @@
               <div class="cc-profile-image"><a href="#"><img src="/data/data_dokter/{{ $dokter->foto }}" /></a></div>
               <div class="h2 title">{{ $dokter->nama_dokter }}</div>
               <p class="capt">{{ $dokter->jabatan}}</p>
-              <!-- <a class="genric-btn primary" href="https://api.whatsapp.com/send?phone=">WhatsApp</a>
-              <a class="genric-btn primary" href="form_konsultasi.php?id_dokter=">Konsultasi</a> -->
+              <a class="genric-btn primary" href="https://api.whatsapp.com/send?phone={{ $dokter->telpon }}">WhatsApp</a>
+              <a class="genric-btn primary" href="/tuliskonsultasi">Konsultasi</a>
             </div>
           </div>
           <div class="section">
